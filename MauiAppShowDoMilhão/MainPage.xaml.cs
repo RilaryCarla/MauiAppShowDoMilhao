f46f0e244fc7d3b61fe3ffefb27dbfbefc8ec104
Usuario <Usuario@CPD03-M12>
2024-09-19 14:30:24 -0300

    titititotototo

--- MauiAppShowDoMilhão/MainPage.xaml.cs
+++ MauiAppShowDoMilhão/MainPage.xaml.cs
@@ -1,13 +1,96 @@
-﻿namespace MauiAppShowDoMilhão
+﻿using MauiAppShowDoMilhão.Models;
+using Plugin.Maui.Audio;
+
+namespace MauiAppShowDoMilhão
 {
     public partial class MainPage : ContentPage
     {
-        double premio = 0;
-        int pergunta_count = 0;
+        double premio = 1000;
+        int pergunta_count = 1;
 
         public MainPage()
         {
             InitializeComponent();
+
+            this.BindingContext = App.getRandomPerguntaFacil();
+
+            lbl_nivel.Text = "Facil";
+            lbl_premio.Text = premio.ToString("C");
+            lbl_pergunta_numero.Text = pergunta_count.ToString();
+
+            //adiciona som
+            Stream track =
+                FileSystem.OpenAppPackageFileAsync("0.mp3").Result;
+            AudioManager.Current.CreatePlayer(track).Play;
+
+        }
+
+        private void toca_som()
+        {
+            string track = "";
+
+            switch(pergunta_count)
+            {
+                case 1:
+                    track = "1000.wav";
+                break;
+
+                case 2:
+                    track = "2000.wav";
+                break;
+
+                case 3:
+                    track = "3000.wav";
+                break;
+
+                case 4:
+                    track = "4000.wav";
+                break;
+
+                case 5:
+                    track = "5000.wav";
+                break;
+
+                case 6:
+                    track = "10000.wav";
+                break;
+
+                case 7:
+                    track = "20000.wav";
+                break;
+
+                case 8:
+                    track = "30000.wav";
+                break;
+
+                case 9:
+                    track = "40000.wav";
+                break;
+
+                case 10:
+                    track = "50000.wav";
+                break;
+
+                case 11:
+                    track = "100000.wav";
+                break;
+
+                case 12:
+                    track = "200000.wav";
+                break;
+
+                case 13:
+                    track = "300000.wav";
+                break;
+
+                case 14:
+                    track = "400000.wav";
+                break;
+
+                case 15:
+                    track = "500000.wav";
+                break;
+            }
         }
 
 
